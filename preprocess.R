25c9c79bf613143c2671961e8412ebff22fc10f4
e.mert <44654977+merterun@users.noreply.github.com>
2023-05-28 19:23:29 +0300

    Add files via upload

---
+++ preprocess.R
@@ -0,0 +1,29 @@
+# Load the necessary libraries
+library(tidyverse)
+library(tm)
+library(SnowballC)
+library(textdata)
+
+# Perform text preprocessing
+processed_text <- books %>%
+  select(title, authors, publisher) %>%  # Select relevant columns
+  mutate(title = tolower(title),  # Convert title to lowercase
+         title = removePunctuation(title),  # Remove punctuation from title
+         title = stripWhitespace(title),  # Remove extra white spaces from title
+         authors = tolower(authors),  # Convert authors to lowercase
+         authors = removePunctuation(authors),  # Remove punctuation from authors
+         authors = stripWhitespace(authors),  # Remove extra white spaces from authors
+         publisher = tolower(publisher),  # Convert publisher to lowercase
+         publisher = removePunctuation(publisher),  # Remove punctuation from publisher
+         publisher = stripWhitespace(publisher))  # Remove extra white spaces from publisher
+
+
+# Perform sentiment analysis
+lexicon <- tidytext::get_sentiments("afinn")  # Load AFINN sentiment lexicon
+
+sentiment_scores <- processed_text %>%
+  unnest_tokens(word, review_text) %>%  # Tokenize the text
+  inner_join(lexicon, by = "word") %>%  # Inner join with sentiment lexicon
+  group_by(bookID) %>%
+  summarise(sentiment = sum(value))
+
